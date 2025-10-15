c3bf193d54275e2cf0d3efe4ea83d7967e856735
MrVihks <viniciushkds@gmail.com>
2025-10-15 00:50:47 -0300

    feat: Adding the authMiddleware

---
+++ backend/src/middlewares/authMiddleware.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+const SECRET = process.env.SECRET || "";
+
+export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) return res.status(401).json({error: "Token ausente ou inválido."});
+
+  const [, token] = authHeader.split(" ");
+  try{
+    const decoded = jwt.verify(token, SECRET) as { id:number }; 
+    (req as any).userId = decoded.id;
+    next();
+  } catch{
+    res.status(401).json({error: "Token inválido."});
+  }
+};
